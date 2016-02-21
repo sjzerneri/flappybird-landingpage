b004f30ff53505a9388bb1b1ce764a1da3d4ca0d
Stevey Z <sjzerneri@gmail.com>
2016-02-21 15:39:32 -0500

    adding more velocity

--- site/js/main.js
+++ site/js/main.js
@@ -2,4 +2,10 @@ $(document).ready(function () {
     $('.test').velocity("fadeIn", {
         duration: 1500
     });
+    $('.head').velocity("slideDown", {
+        duration: 1500
+    });
+    $('.slider').velocity("slideDown", {
+        duration: 1500
+    });
 });
